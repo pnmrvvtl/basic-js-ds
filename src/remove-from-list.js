a5380207b9c6b28a4923161a4eb7929774b09144
pnmrvvtl <pnmrv.vtl@gmail.com>
2022-10-13 17:20:20 +0300

    feat: complete remove from list task

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,9 +22,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  let head = l;
+  while(head.value === k) {
+    head = head.next;
+  }
+
+  let current = head.next;
+  let prev = head;
+  while (current) {
+    if(current.value === k) {
+      prev.next = current.next;
+      current = current.next;
+    } else {
+      prev = current;
+      current = current.next;
+    }
+  }
+  return head;
 }
 
 module.exports = {
